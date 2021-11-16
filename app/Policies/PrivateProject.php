1cac3d17d9722d9ef6f75dfe00fb1fd4211b0fb9
Jacek <kontakt@jk-design.pl>
2021-11-16 12:49:11 +0100

    Poprawki do oznaczania postow, dark mode, poprawki do filtrowania

--- app/Policies/PrivateProject.php
+++ app/Policies/PrivateProject.php
@@ -12,14 +12,8 @@ class PrivateProject
     public function privateProject(User $user, MyProject $privateProject)
     {
         if (Auth::user()->hasRole('Administrator')) {
-
-            dd('admin');
-
             return true;
         } else {
-
-            dd($privateProject->user_id);
-
             return $user->id === $privateProject->user_id
                 ? Response::allow()
                 : Response::deny('You do not own this project.');
